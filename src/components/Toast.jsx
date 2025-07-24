ddcbc69950fae2a48db7d053db13068a7c162792
Dagostocsc <Deborahagosto19@gmail.com>
2025-07-24 00:52:07 -0400

    Added props, still in progress

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Toast = () => {
   return (
-    <div>Toast</div>
-  )
-}
+    <ToastContainer
+      position="top-right"
+      autoClose={3000} //meaning when you get the notification it will close after 3 seconds
+      hideProgressBar={false}
+      closeOnClick //it'll close when you click on it
+      draggable //this will just let you click and rag the notification anywhere over the user interface
+      pauseOnHover
+      theme="dark"
+    />
+  );
+};
 
-export default Toast
+export default Toast;
+
+//This component is like a billboard where all your toast notifications appear.
+//It handles rendering and positioning of all toast messages globally.
+//The file is technically a wrapper component for the ToastContainer from the react-toastify library.
