1b823a321a7b04c23dabf89e3bbbc923b86280e3
RamsesCode <RamsesSanchez25@gmail.com>
2025-07-24 23:12:22 -0400

    reverting dashboard changes after the proper deployment

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { API_URL } from "../shared";
 
-const Account = () => {
+const Dashboard = () => {
   const [polls, setPolls] = useState([]);
   const [statusFilter, setStatusFilter] = useState("all");
   const [search, setSearch] = useState("");
@@ -142,4 +142,4 @@ const Account = () => {
   );
 };
 
-export default Account;
+export default Dashboard;
