fb26de6c58a1470c2c0ca6ffa02425882c1cd263
TJordan77 <tvon777@gmail.com>
2025-07-27 01:02:01 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,45 +5,47 @@ import "./NavBarStyles.css";
 
 const NavBar = ({ user, onLogout }) => {
   return (
-    <nav className="navbar">
-      <div className="nav-brand">
-        <Link to="/">InstaPoll</Link>
-      </div>
-      <div className="nav-links">
-        {/* Always show Home and Create a Poll */}
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-        <Link to="/create" className="nav-link">
-          Create a Poll
-        </Link>
-        {/* Show Dashboard only if logged in */}
-        {user && (
-          <Link to="/dashboard" className="nav-link">
-            Dashboard
+    <div className="navbar-wrapper">
+      <nav className="navbar">
+        <div className="nav-brand">
+          <Link to="/">InstaPoll</Link>
+        </div>
+        <div className="nav-links">
+          {/* Always show Home and Create a Poll */}
+          <Link to="/" className="nav-link">
+            Home
           </Link>
-        )}
-        {/* Show Login/Sign Up if not logged in */}
-        {!user ? (
-          <div className="auth-links">
-            <Link to="/signup" className="nav-link">
-              Sign Up
+          <Link to="/create" className="nav-link">
+            Create a Poll
+          </Link>
+          {/* Show Dashboard only if logged in */}
+          {user && (
+            <Link to="/dashboard" className="nav-link">
+              Dashboard
             </Link>
-            <Link to="/login" className="nav-link">
-              Login
-            </Link>
-          </div>
-        ) : (
-          <div className="user-section">
-            <span className="username">Welcome, {user.username}!</span>
-            <button onClick={onLogout} className="logout-btn">
-              Logout
-            </button>
-          </div>
+          )}
+          {/* Show Login/Sign Up if not logged in */}
+          {!user ? (
+            <div className="auth-links">
+              <Link to="/signup" className="nav-link">
+                Sign Up
+              </Link>
+              <Link to="/login" className="nav-link">
+                Login
+              </Link>
+            </div>
+          ) : (
+            <div className="user-section">
+              <span className="username">Welcome, {user.username}!</span>
+              <button onClick={onLogout} className="logout-btn">
+                Logout
+              </button>
+            </div>
         )}
         <ThemeToggle />
-      </div>
-    </nav>
+        </div>
+      </nav>
+    </div>
   );
 };
 
